ed46f8976650da6ad374f545523b70ae3ee2a212
rishikant42 <rksbtp@gmail.com>
2016-10-13 17:36:35 +0530

    solved ex2.47

--- ch2/ex2.47.scm
+++ ch2/ex2.47.scm
@@ -1,16 +1,16 @@
-(define (make-frame origin edge1 edge2)
-  (list origin edge1 edge2))
-
-(define (origin-frame f) (car f))
-(define (edge1-frame f)  (cadr f))
-(define (edge2-frame f) (caddr f))
-
 ;; (define (make-frame origin edge1 edge2)
-;;   (cons origin (cons edge1 edge2)))
+;;   (list origin edge1 edge2))
 ;; 
 ;; (define (origin-frame f) (car f))
-;; (define (edge1-frame f) (cadr f))
-;; (define (edge2-frame f) (cddr f))
+;; (define (edge1-frame f)  (cadr f))
+;; (define (edge2-frame f) (caddr f))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame f) (car f))
+(define (edge1-frame f) (cadr f))
+(define (edge2-frame f) (cddr f))
 
 (define (make-vect x y)
   (cons x y))
