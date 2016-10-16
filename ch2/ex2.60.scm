bfddb5db5c8fe665578d205fd085912efc6a0a31
rishikant42 <rksbtp@gmail.com>
2016-10-16 15:13:36 +0530

    solved ex2.60

---
+++ ch2/ex2.60.scm
@@ -0,0 +1,20 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin x set)
+    (cons x set))
+
+(define nil '())
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) nil)
+        ((element-of-set? (car set1) set2) (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;; same as append procedure
+(define (union-set set1 set2)
+  (if (null? set1)
+    set2
+    (cons (car set1) (union-set (cdr set1) set2))))
