2d44b55d360bd977aea789fec1c5a4781561fe11
rishikant42 <rksbtp@gmail.com>
2016-10-21 19:13:19 +0530

    added huffman algo problems

---
+++ ch2/huffman-encoding.scm
@@ -0,0 +1,78 @@
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch (choose-branch (car bits) current-branch)))
+        (if (leaf? next-branch)
+          (cons (symbol-leaf next-branch) (decode-1 (cdr bits) tree))
+          (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+    '()
+    (let ((pair (car pairs)))
+      (adjoin-set (make-leaf (car pair) (cadr pair))
+                  (make-leaf-set (cdr pairs))))))
+
+(define (encode message tree)
+  (if (null? message)
+    '()
+    (append (encode-symbol (car message) tree)
+            (encode (cdr message) tree))))
+
+(define (exist? symbol tree)
+  (define (helper items)
+    (cond ((null? items) false)
+          ((equal? (car items) symbol) true)
+          (else (helper (cdr items)))))
+  (helper (symbols tree)))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((exist? symbol tree)
+         (if (exist? symbol (left-branch tree)) 
+           (cons 0 (encode-symbol symbol (left-branch tree)))
+           (cons 1 (encode-symbol symbol (right-branch tree)))))
+        ((error "symbol doesn't present in tree" symbol))))
---
+++ ch2/test.scm
@@ -0,0 +1,22 @@
+(define a (make-leaf 'a 8))
+(define b (make-leaf 'b 3))
+(define c (make-leaf 'c 1))
+(define d (make-leaf 'd 1))
+(define f (make-leaf 'f 1))
+(define e (make-leaf 'e 1))
+(define g (make-leaf 'f 1))
+(define h (make-leaf 'g 1))
+
+(define t1 (make-code-tree c d))
+
+(define t2 (make-code-tree e f))
+
+(define t3 (make-code-tree g h))
+
+(define t5 (make-code-tree t2 t3))
+
+(define t6 (make-code-tree b t1))
+
+(define t7 (make-code-tree t6 t5))
+
+(define t8 (make-code-tree a t7))
