570ba553b95e2fdfe30ffb714c40be623ee58f16
rishikant42 <rksbtp@gmail.com>
2016-10-22 14:46:42 +0530

    added test

--- ch2/eg2.3.3-3.scm
+++ ch2/eg2.3.3-3.scm
@@ -24,3 +24,42 @@
         ((> x (entry set)) (make-tree (entry set)
                                       (left-branch set)
                                       (adjoin-set x (right-branch set))))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;TEST;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 1 ]=> (define t1 (make-tree 1 () ()))
+;; 
+;; ;Value: t1
+;; 
+;; 1 ]=> (define t2 (make-tree 5 () ()))
+;; 
+;; Value: t2
+;; 
+;; 1 ]=> (define left (make-tree 3 t1 t2))
+;; 
+;; ;Value: left
+;; 
+;; 1 ]=> left
+;; 
+;; ;Value 11: (3 (1 () ()) (5 () ()))
+;; 
+;; 1 ]=> (define t3 (make-tree 8 () () ))
+;; 
+;; ;Value: t3
+;; 
+;; 1 ]=> (define right (make-tree 9 t3 () ))
+;; 
+;; ;Value: right
+;; 
+;; 1 ]=> right
+;; 
+;; ;Value 12: (9 (8 () ()) ())
+;; 
+;; 1 ]=> (define tree (make-tree 7 left right))
+;; 
+;; ;Value: tree
+;; 
+;; 1 ]=> tree
+;; 
+;; ;Value 13: (7 (3 (1 () ()) (5 () ())) (9 (8 () ()) ()))
