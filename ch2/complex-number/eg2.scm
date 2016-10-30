4d6856b1f8858ce9dffc0ba69a8b8a3bc644e8e7
rishikant42 <rksbtp@gmail.com>
2016-10-31 01:00:29 +0530

    implement of two diffrent represent coexist using genric op

---
+++ ch2/complex-number/eg2.scm
@@ -0,0 +1,102 @@
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (rectangulra? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
+;; rectangular form
+
+(define (make-from-real-imag-rectangular x y)
+  (attach-tag 'rectangular (cons x y)))
+
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag ' rectangular (cons (* r (cos a)) (* r (sin a)))))
+
+(define (real-part-rectangular z) (car z))
+
+(define (imag-part-rectangular z) (cdr z))
+
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z)) (square (imag-part-rectangular z)))))
+
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z) (real-part-rectangular z)))
+
+;; polar form
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag 'polar (cons (sqrt (+ (square x) (square y))) (atan y x))))
+
+(define (make-from-mag-ang-polar r a)
+  (attach-tag 'polar (cons r a)))
+
+(define (real-part-polar z)
+  (* (magnitude-polar z) (cos (angle-polar z))))
+
+(define (imag-part-polar z)
+  (* (magnitude-polar z) (sin (angle-polar z))))
+
+(define (magnitude-polar z) (car z))
+
+(define (angle-polar z) (cdr z))
+
+;; generic selector
+
+(define (real-part z)
+  (cond ((eq? (type-tag z) 'rectangular) (real-part-rectangular (contents z)))
+        ((eq? (type-tag z) 'polar) (real-part-polar (contents z)))
+        (else (error "Unknow type -- REAL-PART" z))))
+
+(define (imag-part z)
+  (cond ((eq? (type-tag z) 'rectangular) (imag-part-rectangular (contents z)))
+        ((eq? (type-tag z) 'polar) (imag-part-polar (contents z)))
+        (else (error "Unknow type -- IMAG-PART" z))))
+
+(define (magnitude z)
+  (cond ((eq? (type-tag z) 'rectangular) (magnitude-rectangular (contents z)))
+        ((eq? (type-tag z) 'polar) (magnitude-polar (contents z)))
+        (else (error "Unknown type -- MAGNITUDE" z))))
+
+(define (angle z)
+  (cond ((eq? (type-tag z) 'rectangular) (angle-rectangular (contents z)))
+        ((eq? (type-tag z) 'polar) (angle-polar (contents z)))
+        (else (error "Unknown type -- ANGLE" z))))
+
+;;;;;;;;;;;;;;
+
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+                       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+;;;;;;;;;;;
+
+(define (make-from-real-imag x y)
+  (make-from-real-imag-rectangular x y))
+
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))
