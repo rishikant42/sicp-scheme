1b62981083eb27ec1bdc2223a18e09a85c167e76
rishikant42 <rksbtp@gmail.com>
2016-10-31 12:30:46 +0530

    data directed programming implementation

---
+++ ch2/complex-number/eg3.scm
@@ -0,0 +1,165 @@
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (install-rectangular-package)
+  ;; Internal procedure
+
+  (define (make-from-real-imag x y) (cons x y))
+  
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  (define (real-part z) (car z))
+
+  (define (imag-part z) (cdr z))
+
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z)) (square (imag-part z)))))
+
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+
+  ;; Interface to the rest of system
+
+  (define (tag x) (attach-tag 'rectangular x))
+
+  (put 'real-part '(rectangular) real-part)
+
+  (put 'imag-part '(rectangular) imag-part)
+
+  (put 'magnitude '(rectangular) magnitude)
+
+  (put 'angle '(rectangular) angle)
+
+  (put 'make-from-real-imag 'rectangular (lambda (x y) (tag (make-from-real-imag x y))))
+
+  (put 'make-from-mag-ang 'rectangular (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  'done)
+
+(define (install-polar-package)
+  ;; Internal procedure
+
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y))) (atan y x)))
+
+  (define (make-from-mag-ang r a) (cons r a))
+
+  (define (real-part z) (* (magnitude z) (cos (angle z))))
+
+  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+
+  (define (magnitude z) (car z))
+
+  (define (angle z) (cdr z))
+
+  ;; Interface to the rest of system
+
+  (define (tag x) (attach-tag 'polar x))
+
+  (put 'real-part '(polar) real-part)
+
+  (put 'imag-part '(polar) imag-part)
+
+  (put 'magnitude '(polar) magnitude)
+
+  (put 'angle '(polar) angle)
+
+  (put 'make-from-real-imag 'polar (lambda (x y) (tag (make-from-real-imag x y))))
+
+  (put 'make-from-mag-ang 'polar (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  'done)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (error "No method for these types -- APPLY-GENERIC" (list op type-tags))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;; Generic selector ;;;;;;;;;
+
+(define (real-part z) (apply-generic 'real-part z))
+
+(define (imag-part z) (apply-generic 'imag-part z))
+
+(define (magnitude z) (apply-generic 'magnitude z))
+
+(define (angle z) (apply-generic 'angle z))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+                       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;; form section 3.3.3 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (cdr record)
+              false))
+          false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+          (set-cdr! local-table
+                    (cons (list key-1
+                                (cons key-2 value))
+                          (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation - TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
