1b513149b18fd53fc4db6bb1b11b340c9ea5be6e
rishikant42 <rksbtp@gmail.com>
2016-12-14 11:58:27 +0530

    solved ex3.13

---
+++ ch3/exercise/ex3.13.scm
@@ -0,0 +1,13 @@
+(define (last-pair x)
+  (if (null? (cdr x))
+    x
+    (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+;; => z
+;; It will produce infinite recursion
