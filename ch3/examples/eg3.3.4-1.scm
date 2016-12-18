d89767e972a938eb08ee1ebff7ae38747454e8e4
rishikant42 <rksbtp@gmail.com>
2016-12-18 22:53:50 +0530

    added prob procedure

--- ch3/examples/eg3.3.4-1.scm
+++ ch3/examples/eg3.3.4-1.scm
@@ -1,3 +1,5 @@
+(define (make-agenda) (list 0))
+
 (define (make-wire)
   (let ((signal-value 0) (action-procedures '()))
     (define (set-my-signal! new-value)
@@ -15,7 +17,7 @@
             ((eq? m 'set-signal!) set-my-signal!)
             ((eq? m 'add-action!) accept-action-procedure!)
             (else (error "Unknown operation -- WIRE" m))))
-    
+
     dispatch))
 
 (define (call-each procedures)
@@ -24,21 +26,31 @@
     (begin ((car procedures))
            (call-each (cdr procedures)))))
 
-(define (get-signal value)
+(define (get-signal wire)
   (wire 'get-signal))
 
 (define (set-signal! wire new-value)
   ((wire 'set-signal!) new-value))
 
 (define (add-action! wire action-procedure)
-  ((wire 'add-action) action-procedure))
+  ((wire 'add-action!) action-procedure))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (inverter input output)
   (define (invert-input)
     (let ((new-value (logic-not (get-signal input))))
-      (after-delay invert-delay
+      (after-delay inverter-delay
                    (lambda ()
                      (set-signal! output new-value)))))
   (add-action! input invert-input))
@@ -47,3 +59,164 @@
   (cond ((= s 0) 1)
         ((= s 1) 0)
         (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+            (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        (else 0)))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+            (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+
+(define (logical-or s1 s2)
+  (cond ((= s1 1) 1)
+        ((= s2 1) 1)
+        (else 0)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+    'done
+    (let ((first-item (first-agenda-item the-agenda)))
+      (first-item)
+      (remove-first-agenda-item! the-agenda)
+      (propagate))))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
+(define the-agend (make-agenda))
+
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-agenda) (list 0))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+      (insert-queue! (segment-queue (car segments))
+                     action)
+      (let ((rest (cdr segments)))
+        (if (belongs-before? rest)
+          (set-cdr!
+            segments
+            (cons (make-new-time-segment time action)
+                  (cdr segments)))
+          (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+      (set-segments!
+        agenda
+        (cons (make-new-time-segment time action)
+              segments))
+      (add-to-segments! segments))))
+
+
+(define (current-time agenda) (car agenda))
+
+(define the-agenda (make-agenda))
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+      (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+    (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+    (let ((first-seg (first-segment agenda)))
+      (set-current-time! agenda (segment-time first-seg))
+      (front-queue (segment-queue first-seg)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (front-ptr queue) (car queue))
+
+(define (rear-ptr queue) (cdr queue))
+
+(define (set-front-ptr! queue item) (set-car! queue item))
+
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (make-queue) (cons '() '()))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+    (error "FRONT called with an empty queue" queue)
+    (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (if (empty-queue? queue)
+      (begin (set-front-ptr! queue new-pair)
+             (set-rear-ptr! queue new-pair)
+             queue)
+      (begin (set-cdr! (rear-ptr queue) new-pair)
+             (set-rear-ptr! queue new-pair)
+             queue))))
+
+(define (delete-queue! queue)
+  (if (empty-queue? queue)
+    (error "DELETE! called with an empty queue" queue)
+    (begin (set-front-ptr! queue (cdr (front-ptr queue)))
+           queue)))
