c0e41a7101f73bd1cb214add8e0482344c32ce25
rishikant42 <rksbtp@gmail.com>
2016-12-25 20:08:23 +0530

    added ex3.38

---
+++ ch3/exercise/ex3.38.scm
@@ -0,0 +1,8 @@
+;; order                                         balance
+;; 
+;; peter paul marry                              45
+;; peter marry paul                              35
+;; paul peter marry                              45
+;; paul marry petter                             50
+;; marry peter paul                              40
+;; marry paul peter                              40
