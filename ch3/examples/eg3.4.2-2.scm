d9f25ffa1f07ffb72059674878d54559ac8ec6fa
rishikant42 <rksbtp@gmail.com>
2017-01-02 21:33:59 +0530

    implemented bank account with serializer

---
+++ ch3/examples/eg3.4.2-2.scm
@@ -0,0 +1,50 @@
+(load "parallel.scm")
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+               (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+    true
+    (begin (set-car! cell true)
+           false)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+
+  (define (deposite amount)
+    (set! balance (+ balance amount))
+    balance)
+
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposite) (protected deposite))
+            ((eq? m 'balance) balance)
+            (else (error "Unknow request -- MAKE-ACCOUNT"))))
+    dispatch))
