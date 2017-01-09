dc993195692cbc77b8b759211feecfbfb5dbe741
rishikant42 <rksbtp@gmail.com>
2017-01-09 09:28:46 +0530

    added some more stream op

--- ch3/examples/eg3.5.1-2.scm
+++ ch3/examples/eg3.5.1-2.scm
@@ -26,6 +26,18 @@
   (newline)
   (display x))
 
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+    the-empty-stream
+    (cons-stream low
+                 (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream)) (cons-stream (stream-car stream)
+                                                 (stream-filter pred (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
 (define (cons-stream a b)
   (cons a (delay b)))
 
