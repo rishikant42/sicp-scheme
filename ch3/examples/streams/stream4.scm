07a6af8ad9d2ee57e2ea49d1f3e4e27e04c28848
rishikant42 <rksbtp@gmail.com>
2017-01-24 17:04:31 +0530

    practice of ex3.52

---
+++ ch3/examples/streams/stream4.scm
@@ -0,0 +1,78 @@
+(define stream-null? null?)
+
+(define the-empty-stream '())
+
+(define (stream-ref s n)
+  (if (= n 0)
+    (stream-car s)
+    (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+    the-empty-stream
+    (cons-stream (proc (stream-car s))
+                 (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+    'done
+    (begin (proc (stream-car s))
+           (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+    the-empty-stream
+    (cons-stream low
+                 (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream)) (cons-stream (stream-car stream)
+                                                 (stream-filter pred (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-list . args)
+  (define (helper items)
+    (if (null? items)
+      the-empty-stream
+      (cons-stream (car items)
+                   (helper (cdr items)))))
+  (helper args))
+
+
+(define (cons-stream a b)
+  (cons a (delay b)))
+
+(define (stream-car s) (car s))
+
+(define (stream-cdr s) (force (cdr s)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define sum 0)
+
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+
+;; (display-stream seq)
+
+(define y (stream-filter even? seq))
+
+;; (display-stream y)
+
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+
+;; (stream-ref y 7)
+
+;; (display-stream z)
