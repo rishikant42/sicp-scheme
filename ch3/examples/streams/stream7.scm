0c0cd2b642d49afa71af7a1ddbf6768673a6c922
rishikant42 <rksbtp@gmail.com>
2017-01-26 14:16:27 +0530

    renamed add-stream to add-streams

--- ch3/examples/streams/stream7.scm
+++ ch3/examples/streams/stream7.scm
@@ -1,13 +1,13 @@
 (define ones (cons-stream 1 ones))
 
-(define (add-stream s1 s2)
+(define (add-streams s1 s2)
   (stream-map + s1 s2))
 
-(define integers (cons-stream 1 (add-stream ones integers)))
+(define integers (cons-stream 1 (add-streams ones integers)))
 
 (define fibs (cons-stream 0
                           (cons-stream 1
-                                       (add-stream (stream-cdr fibs) fibs))))
+                                       (add-streams (stream-cdr fibs) fibs))))
 
 (define (scale-stream stream factor)
   (stream-map (lambda (x) (* x factor)) stream))
