712c775ae2ac1c3a856bec636da32b4e842fe3e7
rishikant42 <rksbtp@gmail.com>
2017-01-27 11:29:28 +0530

    accelerated pi approximations

--- ch3/examples/streams/stream10.scm
+++ ch3/examples/streams/stream10.scm
@@ -11,5 +11,31 @@
   (cons-stream (/ 1.0 n)
                (stream-map - (pi-summands (+ n 2)))))
 
-(define pi-streams
+(define pi-stream
   (scale-stream (partial-sums (pi-summands 1)) 4))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+;; 1 ]=> (define s (euler-transform pi-stream))
+;; 
+;; ;Value: s
+;; 
+;; 1 ]=> s
+;; 
+;; ;Value 11: (3.166666666666667 . #[promise 12])
+;; 
+;; 1 ]=> (stream-cdr s)
+;; 
+;; ;Value 13: (3.1333333333333337 . #[promise 14])
+;; 
+;; 1 ]=> (stream-cdr (stream-cdr s))
+;; 
+;; ;Value 15: (3.1452380952380956 . #[promise 16])
