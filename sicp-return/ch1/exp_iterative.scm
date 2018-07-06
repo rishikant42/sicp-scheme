cca9b9b7cbda2685c2d798eac4125a90982cbc71
rishikant42 <rksbtp@gmail.com>
2018-07-06 10:31:00 +0530

    added order evaluation for exp procedures

--- sicp-return/ch1/exp_iterative.scm
+++ sicp-return/ch1/exp_iterative.scm
@@ -11,3 +11,17 @@
 
 ;; space complexity = O(1)
 ;; time complexity = O(n)
+
+;; (expt 2 5)
+;;
+;; (expt-iter 2 5 1)
+;;
+;; (expt-iter 2 4 2)
+;;
+;; (expt-iter 2 3 4)
+;;
+;; (expt-iter 2 2 8)
+;;
+;; (expt-iter 2 1 16)
+;;
+;; (expt-iter 2 0 32)
--- sicp-return/ch1/exp_log.scm
+++ sicp-return/ch1/exp_log.scm
@@ -7,3 +7,23 @@
 
 ;; space complexity = O(log(n))
 ;; time complexity = O(log(n))
+
+;; (expt 2 5)
+;;
+;; (* 2 (expt 2 4))
+;;
+;; (* 2 (square (expt 2 2)))
+;;
+;; (* 2 (square (square (expt 2 1))))
+;;
+;; (* 2 (square (square (* 2 (expt 2 0)))))
+;;
+;; (* 2 (square (square (* 2 1))))
+;;
+;; (* 2 (square (square 2)))
+;;
+;; (* 2 (square 4))
+;;
+;; (* 2 16)
+;;
+;; 32
