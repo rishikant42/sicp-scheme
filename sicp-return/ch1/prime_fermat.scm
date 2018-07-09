8443f0a254492a034163d6f47f2ec8b0b0630b40
rishikant42 <rksbtp@gmail.com>
2018-07-09 14:48:42 +0530

    added prime procedure

---
+++ sicp-return/ch1/prime_fermat.scm
@@ -0,0 +1,15 @@
+;; fermat test
+
+;; b^n mod m
+(define (expmod b n m)
+  (remainder (expt b n) m))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
